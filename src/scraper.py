012ff9d85561be5204054facae12f822833a6ef5
gzappa <gzappazg@gmail.com>
2026-03-10 06:37:15 +0200

    Enhance scraper functionality: add get_book_urls to retrieve absolute book URLs and update get_book_list to include book URLs in the output

--- src/scraper.py
+++ src/scraper.py
@@ -5,19 +5,36 @@ import os
 from urllib.parse import urljoin
 import re
 
+BASE_URL = "https://books.toscrape.com/"
 
 def get_soup(url):
     res = requests.get(url)
     return BeautifulSoup(res.text, "html.parser")
 
+def get_book_urls(page_url=BASE_URL):
+    """
+    returns a list of absolute URLs for each book on the page
+    """
+    soup = get_soup(BASE_URL)
+
+    book_urls = []
+    # each book is in an <article class="product_pod"> tag
+    for book in soup.find_all("article", class_="product_pod"):
+        relative_url = book.h3.a["href"]  # relative URL like "catalogue/a-light-in-the-attic_1000/index.html"
+        absolute_url = urljoin(BASE_URL + "catalogue/", relative_url)  # convert to complete URL
+        book_urls.append(absolute_url)
+
+    return book_urls
+
+
 # function to scrape book data from the website and return a list of dictionaries
 def get_book_list():
-    BASE_URL = "https://books.toscrape.com/"
     soup = get_soup(BASE_URL)
+    books_urls = get_book_urls()  # get list of book URLs to ensure we have the correct order and count
     books_html = soup.find_all("article", class_="product_pod")
 
     book_list = []
-    for book in books_html:
+    for i, book in enumerate(books_html):
         title = book.h3.a["title"]
         
         price_text = book.find("p", class_="price_color").text
@@ -33,9 +50,15 @@ def get_book_list():
             "title": title,
             "price": price,
             "rating": rating,
-            "image_url": image_url
+            "image_url": image_url,
+            "book_url": books_urls[i]  # Add the corresponding book URL from the list
+
         })
     return book_list
+
+
+
+
 
 
 if __name__ == "__main__":
